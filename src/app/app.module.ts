ff6bc55bfa9503e10d773abbd9bb76d1db5000bc
userAmal <amalbouaouina6@gmail.com>
2023-10-08 15:27:23 +0200

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProduitsComponent } from './produits/produits.component';
 import { AddProduitComponent } from './add-produit/add-produit.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AddProduitComponent } from './add-produit/add-produit.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/produits/produits.component.ts
+++ src/app/produits/produits.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Produit } from '../model/produit.model';
+import { ProduitService } from '../service/produit.service';
 
 @Component({
   selector: 'app-produits',
   templateUrl: './produits.component.html'
 })
 export class ProduitsComponent implements OnInit {
-  produits : string[];
-  constructor(){
-    this.produits=["PC asus","Imprimante Epson","Tablette samsung"]
+  produits ?: Produit[]; //un tableau de Produit
+  private produitService= new  ProduitService;
+  constructor() {
+  //this.produits=[];
   }
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
+  ngOnInit(): void{
+    this.produits = this.produitService.listeProduits();
   }
+
+  
 }
